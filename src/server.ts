2b2ac681d9be26fef66e7023fc797fd2415054ce
Levi <levi.ciarrocchi@gmail.com>
2021-04-24 12:47:43 -0300

    Aula 02

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
+import "./database";
+
+import { routes } from "./routes";
+
 const app = express();
 
 app.use(express.json());
 
-app.post("/users", (reques, response) => {
-  return response.json({ message: "Usuário salvo com sucesso!" });
-});
+app.use(routes);
 
 app.listen(3000, () => console.log("Server rodando!"));
